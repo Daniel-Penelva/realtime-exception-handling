6b7406b2e735503bf28ba0b3619b33ae3c42605c
Daniel Penelva <d4n.andrade@gmail.com>
2023-12-08 17:18:47 -0300

    Criando projeto Spring Boot 3 para utilizar exception handling

---
+++ src/main/java/com/api/realtimeexceptionhandling/RealtimeExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.api.realtimeexceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeExceptionHandlingApplication.class, args);
+	}
+
+}
